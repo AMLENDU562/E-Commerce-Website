f5b6c3adf35f66841c70e8b9f62a3127c82b2190
AMLENDU562 <amlendu786.562@gmail.com>
2024-07-12 22:04:33 +0530

    improved add to cart

--- fronted-app/src/pages/Cart.jsx
+++ fronted-app/src/pages/Cart.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import products from './products'
+import React, { useState, useEffect } from 'react';
+import products from './products';
 
 export default function Cart() {
-  const localStorageItems = Object.keys(localStorage).map((key) => {
-    return { key: key, value: localStorage.getItem(key) };
-  });
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const items = Object.keys(localStorage).map((key) => {
+      return { key: key, value: localStorage.getItem(key) };
+    });
+    setCartItems(items);
+  }, []);
+
+  function deleteKey(key) {
+    localStorage.removeItem(key);
+    setCartItems(cartItems.filter(item => item.key !== key));
+  }
 
   return (
     <div>
       <h1 style={{ fontSize: "3em", color: "white", paddingLeft: "20px" }}>Shopping Cart 🛒</h1>
       <hr></hr>
       <ul>
-        {localStorageItems.map((item, index) => (
-          <img src={products[item.key-1].img} alt={products[item.key-1].text}/>
-        ))}
+        {cartItems.map((item, index) => {
+          const product = products[item.key - 1];
+          return product ? (
+            <div key={item.key}>
+              <img src={product.img} alt={product.text} />
+              <button
+                id='remove'
+                onClick={() => deleteKey(item.key)}
+                style={{ margin: "5px", cursor: "pointer" }}
+              >
+                -
+              </button>
+            </div>
+          ) : null;
+        })}
       </ul>
     </div>
   );
--- fronted-app/src/pages/Product.jsx
+++ fronted-app/src/pages/Product.jsx
@@ -8,17 +8,8 @@ export default function Product({ img, text, id , onAdd, onDelete }) {
     <div className="Product" style={{ padding: "5px" }}>
       <img src={img} alt={text} style={{ height: "250px", width: "200px" }} />
       <h1 style={{color:"black"}}>{text}</h1>
-      <button 
-        onClick={() => onAdd(id)} id='add'
-        style={{ margin: "5px", cursor: "pointer" }}
-      >
-        +
-      </button>
-      <button id='remove'
-        onClick={() => onDelete(id)} 
-        style={{ margin: "5px", cursor: "pointer" }}
-      >
-        -
+      <button onClick={() => onAdd(id)} id='add' style={{ margin: "5px", cursor: "pointer" }}>+</button>
+      <button id='remove'onClick={() => onDelete(id)} style={{ margin: "5px", cursor: "pointer" }}>-
       </button>
     </div>
   );
