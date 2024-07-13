e5197aa0fdfe138e8c0ceed464ded25933c6c1bb
AMLENDU562 <amlendu786.562@gmail.com>
2024-07-13 12:57:45 +0530

    changes made

--- fronted-app/src/pages/Cart.jsx
+++ fronted-app/src/pages/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import products from './products';
+import { useNavigate } from "react-router-dom";
 import '../Cart.css'; // Assuming you will create this CSS file for additional styling
 
 export default function Cart() {
+  const navigate = useNavigate();
   const [cartItems, setCartItems] = useState([]);
+  const [count, setCount] = useState(localStorage.length);
 
   useEffect(() => {
     const items = Object.keys(localStorage).map((key) => {
@@ -14,21 +17,33 @@ export default function Cart() {
 
   function deleteKey(key) {
     localStorage.removeItem(key);
+    setCount(localStorage.length);
     setCartItems(cartItems.filter(item => item.key !== key));
   }
 
   return (
     <div className="cart-container">
       <h1 className="cart-title">Shopping Cart 🛒</h1>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <a href="/" className="nav-list-link">Home</a>
+          </li>
+          <li className="nav-list-item">
+            <button onClick={() => navigate('/cart')} id='addToCart'>
+              🛒 {count}
+            </button>
+          </li>
+        </ul>
+      </nav>
       <hr />
       <ul className="cart-list">
-        {cartItems.map((item, index) => {
+        {cartItems.map((item) => {
           const product = products[item.key - 1];
           return product ? (
             <li key={item.key} className="cart-item">
               <img src={product.img} alt={product.text} className="cart-item-image" />
               <div className="cart-item-details">
-                
                 <h2 className="cart-item-title">{product.text}</h2>
                 <button
                   id='remove'
@@ -38,8 +53,7 @@ export default function Cart() {
                   Remove
                 </button>
               </div>
-              <h2 style={{paddingLeft:"50px"}}>₹ {product.cost}</h2>
-
+              <h2 className="cart-item-price">₹ {product.cost}</h2>
             </li>
           ) : null;
         })}
